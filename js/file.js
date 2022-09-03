2f76fe8f03d66743cf12ac806583c218f8567ba2
tazrian-amin <amin.tazrian1979@gmail.com>
2022-09-03 15:52:53 +0600

    navbar added

--- js/file.js
+++ js/file.js
@@ -1,1 +1,17 @@
-console.log('js file');
+document.getElementById('breaking-news-btn').addEventListener("click", function () {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadNews(data.data.news_category[0].category_id))
+})
+
+const loadNews = (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
+
+const displayNews = () => {
+    const newsContainer = document.getElementById('news-container');
+}
